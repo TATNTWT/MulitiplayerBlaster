fd41a32bd193c00c540bff2814092575f6158614
TATNTWT <102528823+TATNTWT@users.noreply.github.com>
2024-11-01 20:28:35 +0800

    Create LobbyGameMode.cpp

---
+++ Source/Blaster/GameMode/LobbyGameMode.cpp
@@ -0,0 +1,19 @@
+#include "LobbyGameMode.h"
+#include "GameFramework/GameStateBase.h"
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	//获取玩家数量，玩家数大于2时ServerTravel到游戏地图
+	int32 NumberOfPlayer = GameState->PlayerArray.Num();
+	if (NumberOfPlayer == 2)
+	{
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			bUseSeamlessTravel = true;
+			World->ServerTravel(FString("/Game/Maps/BlasterMap?listen"));
+		}
+	}
+}
