53a9613494f5a2a5680f441fa4fecc5e1efe9291
TATNTWT <102528823+TATNTWT@users.noreply.github.com>
2024-11-01 20:28:59 +0800

    Create OverheadWidget.cpp

---
+++ Source/Blaster/HUD/OverheadWidget.cpp
@@ -0,0 +1,16 @@
+#include "OverheadWidget.h"
+#include "Components/TextBlock.h"
+
+void UOverheadWidget::SetDisplayText(FString TextToDisplay)
+{
+	if (DisplayText)
+	{
+		DisplayText->SetText(FText::FromString(TextToDisplay));
+	}
+}
+
+void UOverheadWidget::OnLevelRemovedFromWorld(ULevel* InLevel, UWorld* InWorld)
+{
+	RemoveFromParent();
+	Super::OnLevelRemovedFromWorld(InLevel, InWorld);
+}
